ccfe3e24c4c62521313431c387d729f011beaf7a
Afif Zafri <zafri_21596@yahoo.com>
2018-11-12 23:11:44 +0800

    convert image from RGB to grayscale

--- main.m
+++ main.m
@@ -5,3 +5,8 @@ Inputimage=imread('example.jpg');
 figure(1)
 imshow(Inputimage);
 title('ORIGINAL IMAGE')
+
+%% Convert to gray scale
+if size(Inputimage,3)==3 % RGB image
+ Inputimage=rgb2gray(Inputimage);
+end
