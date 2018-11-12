ac3f36b1fa2114c6344d1bf6182e29cf7741c26d
Afif Zafri <zafri_21596@yahoo.com>
2018-11-12 23:29:29 +0800

    test calling main script inside UI

--- ui.m
+++ ui.m
@@ -85,3 +85,4 @@ function extractBtn_Callback(hObject, eventdata, handles)
 % hObject    handle to extractBtn (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
+main;
